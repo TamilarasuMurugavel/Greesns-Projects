223f97a6121878a117d2cc93d19dcae1fa1859e3
TamilarasuMurugavel <tamilarasumurugavel@gmail.com>
2021-09-04 06:53:41 +0530

    gereens 1st project

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,25 @@
+package org.company;
+
+public class CompanyInfo {
+	private void companyName() {
+		// TODO Auto-generated method stub
+		System.out.println("Company name is Greens");
+
+	}
+	private void companyId() {
+		// TODO Auto-generated method stub
+		System.out.println("Company Id is 1234");
+
+	}private void companyAddress() {
+		// TODO Auto-generated method stub
+		System.out.println("Company Address is Chennai");
+
+	}
+	public static void main(String[] args) {
+		CompanyInfo a=new CompanyInfo();
+		a.companyId();
+		a.companyName();
+		a.companyAddress();
+	}
+
+}
---
+++ src/org/demo/demo.java
@@ -0,0 +1,20 @@
+package org.demo;
+
+public class demo {
+
+	private void methodTwo() {
+		
+	System.out.println("10");	
+	}
+	
+	private void methodOne() {
+System.out.println("20");
+	}
+	
+	private void methodThree() {
+		// Tyty8ODO Auto-generated method stub
+System.out.println("40");
+	}
+	
+	
+}
